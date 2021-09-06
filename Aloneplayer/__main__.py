38fe3f690c3d955ed45f2ec989e14ea1a1be7356
ALONE BOY <84889578+PRONOI@users.noreply.github.com>
2021-09-06 20:41:38 +0530

    Update __main__.py

--- Aloneplayer/__main__.py
+++ Aloneplayer/__main__.py
@@ -1,5 +1,5 @@
-# AdityaPlayer (Telegram bot project)
-# Copyright (C)  Aditya Halder
+# Aloneplayer (Telegram bot project)
+# Copyright (C)  @ALONExBOY
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
