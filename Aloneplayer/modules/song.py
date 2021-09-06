442d36569d0dbb9aef3047ced7a65949f7eb909c
ALONE BOY <84889578+PRONOI@users.noreply.github.com>
2021-09-06 19:47:48 +0530

    Update song.py

--- Aloneplayer/modules/song.py
+++ Aloneplayer/modules/song.py
@@ -35,8 +35,8 @@ from pyrogram.types import Message
 from youtube_search import YoutubeSearch
 from youtubesearchpython import SearchVideos
 
-from AdityaPlayer.config import DURATION_LIMIT
-from AdityaPlayer.modules.play import arq
+from Aloneplayer.config import DURATION_LIMIT
+from Aloneplayer.modules.play import arq
 
 
 @Client.on_message(filters.command("song") & ~filters.channel)
