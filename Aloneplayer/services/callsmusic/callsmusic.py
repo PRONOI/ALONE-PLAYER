fb290d8ae92cbd7dbcffafccbfa19bae44e0528e
Shinchan7222 <83214906+Shinchan7222@users.noreply.github.com>
2021-09-07 11:51:45 +0530

    Update callsmusic.py

--- Aloneplayer/services/callsmusic/callsmusic.py
+++ Aloneplayer/services/callsmusic/callsmusic.py
@@ -18,8 +18,8 @@
 from pyrogram import Client
 from pytgcalls import PyTgCalls
 
-from AdityaPlayer.config import API_HASH, API_ID, SESSION_NAME
-from AdityaPlayer.services.queues import queues
+from Aloneplayer.config import API_HASH, API_ID, SESSION_NAME
+from Aloneplayer.services.queues import queues
 
 client = Client(SESSION_NAME, API_ID, API_HASH)
 pytgcalls = PyTgCalls(client)
