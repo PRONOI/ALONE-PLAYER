3735c760d024f9e4388ba3f0e3948573827b492e
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-04-30 21:21:38 +0530

    Update filters.py

--- Aloneplayer/helpers/filters.py
+++ Aloneplayer/helpers/filters.py
@@ -1,5 +1,5 @@
-# Aditya Player - Telegram bot for streaming audio in group calls
-# Copyright (C) 2021  Aditya Halder
+# Calls Music 1 - Telegram bot for streaming audio in group calls
+# Copyright (C) 2021  Roj Serbest
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
